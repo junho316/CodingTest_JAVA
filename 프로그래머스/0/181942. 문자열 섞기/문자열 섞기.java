965413087d5972ff3454e0e7049f440394f7da61
junho316 <108105037+junho316@users.noreply.github.com>
2024-04-27 01:03:36 +0900

    [level 0] Title: 문자열 섞기, Time: 15.41 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,9 +1,21 @@
+import java.util.*;
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
+        ArrayList<String> temp1 = new ArrayList<String>();
+        ArrayList<String> temp2 = new ArrayList<String>();
+        
         for (int i = 0; i < str1.length(); i++){
-            answer = answer + str1.substring(i, i+1) + str2.substring(i, i+1);
+            temp1.add(str1.substring(i, i+1));
         }
+        for (int i = 0; i < str2.length(); i++){
+            temp2.add(str2.substring(i, i+1));
+        }
+        
+        for (int i = 0; i < temp1.size(); i++){
+            answer += temp1.get(i) + temp2.get(i); 
+        }
+        
         return answer;
     }
 }
